765f653fe51a26dc49867bbbc5ad6e632a598117
John Barker <jabarker1@gmail.com>
2013-03-08 14:39:11 -0800

    Fixed numerizer load error.

--- lib/tickle/tickle.rb
+++ lib/tickle/tickle.rb
@@ -19,6 +19,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+require_relative '../numerizer/numerizer.rb'
+
 module Tickle  #:nodoc:
   class << self #:nodoc:
     # == Configuration options
